87521e6cfde8c6c258e3f07fb4e350b680160806
Nata-Sd <angelicacb6392@gmail.com>
2026-05-28 17:31:11 -0500

    first commit

---
+++ CAP 1 INTRODUCCION/EX01/Ejercicio01.js
@@ -0,0 +1,2 @@
+document.write( "Hola Mundo" ); //esto se ve en el DOM
+console.log("Hola Mundo"); // Esto se ve en la consola del navegador
---
+++ CAP 1 INTRODUCCION/EX02/ex02.js
@@ -0,0 +1,6 @@
+var nombre; //declaracion de la variable "nombre"
+nombre = "juan"; // Asignacion de un valor de la variable "nombre" en la consola
+console.log(nombre); // reasignacion de un valor a la variable "nombre" en la consola
+
+var nombre="maria"; //reasignacion de un nuvo valor a la variable "nombre"
+console.log(nombre);
